1dee6f171dc82b7bfe4de8e53dc0224ab38aea84
Svenja Dunger <svenjadunger@web.de>
2024-02-29 19:59:50 +0100

    install mongodb

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from "express";
 const app = express();
-const port = process.env.PORT || 5001;
+const port = process.env.PORT || 5011;
 app.listen(port, () => {
   console.log("Server is running on port" + port);
 });
