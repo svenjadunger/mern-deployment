11aa2fc0573a3f19a3d21c0f2373db54578a2ee2
Svenja Dunger <svenjadunger@web.de>
2024-03-05 09:55:46 +0100

    delete unused files

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,7 +6,6 @@ function App() {
 
   return (
     <>
-      <h1>Was geht</h1>
       <p>Hi</p>
       <Plant/>
     </>
--- client/src/components/Plant.tsx
+++ client/src/components/Plant.tsx
@@ -1,36 +1,44 @@
-// components/Plant.tsx
 import React, { useEffect, useState } from "react";
 
-type Plant = {
+type PlantType = {
   _id: string;
-    name: string;
-    family: string;
+  commonName?: string; // Mark as optional since not all plants have a common name
+  family: string;
+  scientificNameWithAuthor: string;
+  symbol?: string;
+  synonymSymbol?: string;
 };
 
-const Plant: React.FC = () => {
-  const [plants, setPlants] = useState<Plant[]>([]);
+const PlantList: React.FC = () => {
+  const [plants, setPlants] = useState<PlantType[]>([]);
 
- useEffect(() => {
-   fetch("http://localhost:3000/plant/") 
-     .then((response) => response.json())
-     .then((data) => setPlants(data))
-     .catch((error) => console.error("Fetching plants failed:", error));
- }, []);
-
+  useEffect(() => {
+    fetch("http://localhost:3000/plant/65e6126882cafc76e2da8289")
+      .then((response) => response.json())
+      .then((data) => {
+        setPlants(data);
+        console.log("Number of fetched plants:", data.length);
+      })
+      .catch((error) => console.error("Fetching plants failed:", error));
+  }, []);
 
   return (
     <table>
       <thead>
         <tr>
-          <th>Name</th>
-          
+          <th>Common Name</th>
+          <th>Family</th>
+          <th>Scientific Name</th>
         </tr>
       </thead>
       <tbody>
         {plants.map((plant) => (
           <tr key={plant._id}>
-            <td>{plant.name}</td>
-            
+            <td>{plant.commonName || "N/A"}</td>{" "}
+            {/* Display "N/A" if commonName is missing */}
+            <td>{plant.family || "Unknown"}</td>{" "}
+            {/* Display "Unknown" if family is empty */}
+            <td>{plant.scientificNameWithAuthor}</td>
           </tr>
         ))}
       </tbody>
@@ -38,4 +46,4 @@ const Plant: React.FC = () => {
   );
 };
 
-export default Plant;
+export default PlantList;
