051b717b93d1f35cd9310afeb7c5b97939377259
Svenja Dunger <svenjadunger@web.de>
2024-03-07 13:29:15 +0100

    delete unused react imports

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Plant from "./components/Plant";
 import PlantDetail from "./components/PlantDetail"; 
