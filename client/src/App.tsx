04de2e00afea2538b15c61b95ccfe4d8de1a386a
Svenja Dunger <svenjadunger@web.de>
2024-03-07 09:05:47 +0100

    add Navbar file and import

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,20 +4,7 @@ function App() {
   return (
     <>
       <p className="text-center">Scroll down to see 🪴🌿🌵</p>
-      <style>
-        {`
-          @keyframes spin {
-            from { transform: rotate(0deg); }
-            to { transform: rotate(360deg); }
-          }
-          .spin {
-            animation: spin 2s linear infinite;
-          }
-        `}
-      </style>
-      <div className="flex justify-center items-center min-h-screen">
-        <h1 className="text-7xl spin">I ❤️ Svenja Dunger</h1>
-      </div>
+   
       <Plant />
     </>
   );
