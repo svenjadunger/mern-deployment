e68ceff25d3854b288bb4beeb22ecfdcb66b5e43
Svenja Dunger <svenjadunger@web.de>
2024-03-07 10:00:39 +0100

    server restarted

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,13 @@
 import Plant from "./components/Plant";
 import "../globals.css";
+import { NavigationMenuDemo } from "./components/Navbar";
+
 function App() {
   return (
     <>
+      <NavigationMenuDemo />
       <p className="text-center">Scroll down to see 🪴🌿🌵</p>
-   
+
       <Plant />
     </>
   );
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -9,4 +9,8 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+
+  define: {
+    "process.env": {},
+  },
 });
