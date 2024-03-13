ceb1b1aad60ccbd30a2e5df9ece6a78f15db13c3
Svenja Dunger <svenjadunger@web.de>
2024-03-13 14:25:36 +0100

    add plant description

--- client/src/components/PlantDetail.tsx
+++ client/src/components/PlantDetail.tsx
@@ -5,30 +5,50 @@ interface PlantType {
   commonName: string;
   family?: string;
   scientificNameWithAuthor?: string;
+  description: string;
 }
 
 
 const PlantDetail = () => {
   const [plant, setPlant] = useState<PlantType | null>(null);
-  const { plantId } = useParams(); 
+  const { plantId } = useParams<{ plantId: string }>();
 
   useEffect(() => {
     fetch(`http://localhost:3000/plant/${plantId}`)
       .then((response) => response.json())
-      .then((data) => setPlant(data))
-      .catch((error) => console.error("Error", error));
+      .then((data) => {
+
+        const description =
+          data.family && data.scientificNameWithAuthor
+            ? `The plant ${data.commonName} (${data.scientificNameWithAuthor}) belongs to the ${data.family} family.`
+            : "No detailed description available.";
+
+        setPlant({
+          ...data,
+          description, 
+        });
+      })
+      .catch((error) => console.error("Error:", error));
   }, [plantId]);
 
   if (!plant) return <div>Loading...</div>;
 
   return (
-    <div>
-      <h2>{plant.commonName}</h2>
-
-      {plant.family && <p>Familiy: {plant.family}</p>}
-      {plant.scientificNameWithAuthor && (
-        <p>Name: {plant.scientificNameWithAuthor}</p>
+    <div style={{ padding: "20px", fontFamily: "Arial" }}>
+      <h2 style={{ color: "#33691e" }}>{plant.commonName}</h2>
+      {plant.family && (
+        <p>
+          <strong>Family:</strong> {plant.family}
+        </p>
       )}
+      {plant.scientificNameWithAuthor && (
+        <p>
+          <strong>Scientific Name:</strong> {plant.scientificNameWithAuthor}
+        </p>
+      )}
+      <p>
+        <strong>Description:</strong> {plant.description}
+      </p>
     </div>
   );
 };
