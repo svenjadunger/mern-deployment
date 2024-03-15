8a18f26716eea030d8a0be33e01df3709ed376b6
Svenja Dunger <svenjadunger@web.de>
2024-03-15 13:42:58 +0100

    add login route

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -60,7 +60,7 @@ export function NavigationMenuDemo() {
           </NavigationMenuContent>
         </NavigationMenuItem>
         <NavigationMenuItem>
-          <NavigationMenuTrigger>Login</NavigationMenuTrigger>
+          <NavigationMenuTrigger onClick={() => (window.location.href = "/login")}>Login</NavigationMenuTrigger>
           <NavigationMenuContent>
             <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
               {components.map((component) => (
