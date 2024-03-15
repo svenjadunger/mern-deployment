a68be2c877ece101c73ff32cad3af8227e93e114
Svenja Dunger <svenjadunger@web.de>
2024-03-15 13:34:11 +0100

    implement login component

--- client/src/components/Login.tsx
+++ client/src/components/Login.tsx
@@ -1,32 +1,65 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
+// UI components from the library
+import { Button } from "./ui/button";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+} from "./ui/card";
+import { Input } from "./ui/input";
+import { Label } from "./ui/label";
 
 function Login() {
+  const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [picture, setPicture] = useState<File | null>(null);
+  const [fileName, setFileName] = useState("");
 
-  const handleSubmit = async (e: { preventDefault: () => void; }) => {
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files && e.target.files.length > 0) {
+      setPicture(e.target.files[0]);
+      setFileName(e.target.files[0].name);
+    } else {
+      setPicture(null);
+      setFileName("");
+    }
+  };
+
+  const triggerFileInput = () => {
+    document.getElementById("fileInput")?.click();
+  };
+
+  const handleSubmit = async (e: { preventDefault: () => void }) => {
     e.preventDefault();
+
+    const formData = new FormData();
+    formData.append("firstName", name);
+    formData.append("lastName", name);
+    formData.append("phone", "45837783");
+    formData.append("email", email);
+    formData.append("password", password);
+    if (picture) {
+      formData.append("picture", picture);
+    }
 
     const url = "http://localhost:3000/user/login";
 
     try {
       const response = await fetch(url, {
         method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          email: email,
-          password: password,
-        }),
+        body: formData,
       });
 
       const data = await response.json();
-      if (response.status === 200) {
-        console.log("Login successful", data);
+      if (response.status === 201) {
+        console.log("Account successfully created", data);
       } else {
-        console.error("Login failed", data.error);
+        console.error("Registration failed", data.error);
       }
     } catch (error) {
       console.error("There was an error!", error);
@@ -35,41 +68,80 @@ function Login() {
 
   return (
     <div className="container mx-auto h-screen flex items-center justify-center">
-      <div className="max-w-md w-full space-y-8">
-        <form className="mt-8 space-y-6" onSubmit={handleSubmit}>
-          <input
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            placeholder="Email"
-            required
-            className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-green-500 focus:border-green-500 focus:z-10 sm:text-sm"
-          />
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            placeholder="Password"
-            required
-            className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-green-500 focus:border-green-500 focus:z-10 sm:text-sm"
-          />
-          <button
-            type="submit"
-            className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500"
-          >
-            Login
-          </button>
-        </form>
-        <p className="mt-2 text-center text-sm text-gray-600">
-          Don't have an account?{" "}
-          <Link
-            to="/register"
-            className="font-medium text-green-600 hover:text-green-500"
-          >
-            Sign up
-          </Link>
-        </p>
-      </div>
+      <Card>
+        <CardHeader className="space-y-2">
+          <CardTitle className="text-2xl">Login to your account</CardTitle>
+          <CardDescription>
+            Enter your details below to login to your account
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="grid gap-4">
+          <form className="space-y-4" onSubmit={handleSubmit}>
+            <div className="grid gap-2">
+              {" "}
+              <Label htmlFor="name">Name</Label>
+              <Input
+                id="name"
+                type="text"
+                className="input-align"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                required
+              />
+            </div>
+            <div className="grid gap-2">
+              <Label htmlFor="email">Email</Label>
+              <Input
+                id="email"
+                type="email"
+                className="input-align"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+            </div>
+            <div className="grid gap-2">
+              <Label htmlFor="password">Password</Label>
+              <Input
+                id="password"
+                type="password"
+                className="input-align"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+            </div>
+            <div className="grid gap-2">
+              <Label htmlFor="image">Image</Label>
+              <div>
+                <input
+                  id="fileInput"
+                  type="file"
+                  onChange={handleFileChange}
+                  style={{ display: "none" }}
+                />
+                <Button
+                  type="button"
+                  className="w-full"
+                  onClick={triggerFileInput}
+                >
+                  Choose file
+                </Button>
+                {fileName && (
+                  <div className="mt-2 text-sm text-gray-500">
+                    Selected: {fileName}
+                  </div>
+                )}
+              </div>
+            </div>
+            <Button type="submit" className="w-full">
+              Login
+            </Button>
+          </form>
+          
+        </CardContent>
+        <CardFooter></CardFooter>
+      </Card>
     </div>
   );
 }
